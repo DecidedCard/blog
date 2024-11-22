065ab33bb660691c47e18c96d2a6a8352e879796
heajun jeong <heajun35@gmail.com>
2024-11-22 14:23:21 +0900

    [Feat] - supabase 데이터 가져오는 로직 작성

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,8 +1,19 @@
 import clientSupabase from "@/util/clientSupabase";
 
-import type { Post } from "@/app/page";
+import type { InputForm } from "@/app/page";
 
-export const insertPost = async (post: Post) => {
+export const readPost = async () => {
+  const { data: post, error } = await clientSupabase.from("post").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("데이터를 가져오는 중에 에러가 발생하였습니다.");
+  }
+
+  return post;
+};
+
+export const insertPost = async (post: InputForm) => {
   const { data, error } = await clientSupabase
     .from("post")
     .insert([post])
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,41 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 
 import styles from "@/styles/page.module.scss";
-import { insertPost } from "@/api/supabase";
+import { insertPost, readPost } from "@/api/supabase";
+import { useEffect, useState } from "react";
+
+export interface InputForm {
+  title: string;
+  comment: string;
+}
 
 export interface Post {
+  id: number;
+  created_at: string;
   title: string;
   comment: string;
 }
 
 export default function Home() {
-  const { register, watch, handleSubmit } = useForm<Post>();
+  const { register, handleSubmit } = useForm<InputForm>();
 
-  console.log(watch());
+  const [posts, setPosts] = useState<Post[] | null>(null);
 
-  const onSubmitHandler: SubmitHandler<Post> = async (data) => {
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await readPost();
+
+      if (data) {
+        setPosts(data);
+      }
+    };
+
+    fetch();
+  }, []);
+
+  console.log(posts);
+
+  const onSubmitHandler: SubmitHandler<InputForm> = async (data) => {
     await insertPost(data);
     console.log(data);
   };
@@ -27,6 +49,18 @@ export default function Home() {
         <input type="text" placeholder="내용" {...register("comment")} />
         <button type="submit">등록</button>
       </form>
+
+      {posts && posts.length !== 0 && (
+        <section className={styles.posts}>
+          <h2>포스트</h2>
+          {posts.map((item) => (
+            <div className={styles.post_card} key={item.id}>
+              <p>{item.title}</p>
+              <p>{item.comment}</p>
+            </div>
+          ))}
+        </section>
+      )}
     </div>
   );
 }
