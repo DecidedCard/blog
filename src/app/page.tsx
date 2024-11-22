410213be3d977076601382be1733f431e8b7b8cf
heajun jeong <heajun35@gmail.com>
2024-11-22 13:34:00 +0900

    [Feat] - 작성 폼 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import styles from "@/styles/page.module.scss";
 
 export default function Home() {
-  return <div className={styles.test}>test</div>;
+  return (
+    <div className={styles.test}>
+      <input type="text" placeholder="제목" />
+      ``
+      <input type="text" placeholder="내용" />
+    </div>
+  );
 }
