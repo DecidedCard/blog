9222a3f7aadd5935fad3797e7f3463810d077a9e
heajun jeong <heajun35@gmail.com>
2024-11-22 13:57:00 +0900

    [Test] - react-hook-form onSubmit 함수 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 import styles from "@/styles/page.module.scss";
 
@@ -10,14 +10,21 @@ export interface Post {
 }
 
 export default function Home() {
-  const { register, watch } = useForm<Post>();
+  const { register, watch, handleSubmit } = useForm<Post>();
 
   console.log(watch());
 
+  const onSubmitHandler: SubmitHandler<Post> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.test}>
-      <input type="text" placeholder="제목" {...register("title")} />
-      <input type="text" placeholder="내용" {...register("comment")} />
+      <form onSubmit={handleSubmit(onSubmitHandler)}>
+        <input type="text" placeholder="제목" {...register("title")} />
+        <input type="text" placeholder="내용" {...register("comment")} />
+        <button type="submit">등록</button>
+      </form>
     </div>
   );
 }
