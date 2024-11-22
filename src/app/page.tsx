06e0b31f60e86a0ee0f676460ab6d7ec32660eab
heajun jeong <heajun35@gmail.com>
2024-11-22 13:36:47 +0900

    [Feat] - react-hook-form 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+
 import styles from "@/styles/page.module.scss";
 
+interface WritingForm {
+  title: string;
+  content: string;
+}
+
 export default function Home() {
+  const { register, watch } = useForm<WritingForm>();
+
+  console.log(watch());
+
   return (
     <div className={styles.test}>
-      <input type="text" placeholder="제목" />
-      ``
-      <input type="text" placeholder="내용" />
+      <input type="text" placeholder="제목" {...register("title")} />
+      <input type="text" placeholder="내용" {...register("content")} />
     </div>
   );
 }
