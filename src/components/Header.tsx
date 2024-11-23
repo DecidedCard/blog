83d2627eb35b56c3d7f3485f9972da95674ffb3d
“DecidedCard <heajun35@gmail.com  git config --global user.name DecidedCardgit config --global user.email heajun35@gmail.com>
2024-11-23 20:55:58 +0900

    [Style] - header css 작성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+import styles from "@/styles/header.module.scss";
+
 const Header = () => {
-  return <div>Header</div>;
+  return <header className={styles.header}>나만의 작은 이야기 공간</header>;
 };
 
 export default Header;
