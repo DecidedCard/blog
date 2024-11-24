dda4804277330aa454fe199e05a78269f4211a91
heajun jeong <heajun35@gmail.com>
2024-11-24 16:08:00 +0900

    [Feat] - queryClient defaultOptions 작성

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -5,7 +5,14 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 0,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
