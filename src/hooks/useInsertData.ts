e2182383c9aa71b5c103527eef4bfbf746c86a3a
heajun jeong <heajun35@gmail.com>
2024-11-24 16:28:06 +0900

    [Feat] - data insert mutation 작성

---
+++ src/hooks/useInsertData.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { insertPost } from "@/api/supabase";
+
+import QUERY_KEY from "@/util/QUERY_KEY";
+
+//data 입력 mutation
+const useInsertData = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: insert } = useMutation({
+    mutationFn: insertPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.post] });
+    },
+  });
+
+  return insert;
+};
+
+export default useInsertData;
